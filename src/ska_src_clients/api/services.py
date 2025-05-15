b42324f3b1edcea27a5ab00db3a08a0c08a5678d
rob barnsley <rob.barnsley@skao.int>
2025-05-15 15:15:43 +0100

    adding better exception handling

--- src/ska_src_clients/api/services.py
+++ src/ska_src_clients/api/services.py
@@ -1,10 +1,12 @@
 from ska_src_clients.api.api import API
+from ska_src_clients.common.exceptions import handle_client_exceptions
 
 
 class ServicesAPI(API):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    @handle_client_exceptions
     def health(self, service):
         """ Get the health of a service.
 
@@ -13,6 +15,7 @@ class ServicesAPI(API):
         client = self.session.client_factory.get_client_from_service_name(service)
         return client.health().json()
 
+    @handle_client_exceptions
     def ping(self, service):
         """ Ping a service.
 
--- src/ska_src_clients/api/site.py
+++ src/ska_src_clients/api/site.py
@@ -4,6 +4,7 @@ import logging
 from fastapi.exceptions import HTTPException
 
 from ska_src_clients.api.api import API
+from ska_src_clients.common.exceptions import handle_client_exceptions
 
 
 class SiteAPI(API):
@@ -12,6 +13,7 @@ class SiteAPI(API):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    @handle_client_exceptions
     def disable_service(self, service_uuid):
         """ Disable a service by service uuid.
 
@@ -20,6 +22,7 @@ class SiteAPI(API):
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         return client.set_service_disabled(service_uuid=service_uuid)
 
+    @handle_client_exceptions
     def enable_service(self, service_id):
         """ Enable a service by service uuid.
 
@@ -29,11 +32,13 @@ class SiteAPI(API):
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         return client.set_service_enabled(service_uuid=service_uuid)
 
+    @handle_client_exceptions
     def get_add_node_www_url(self):
         """ Get the add site www URL. """
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         return client.get_add_node_www_url()
 
+    @handle_client_exceptions
     def get_edit_node_www_url(self, node_name):
         """ Get the edit site www URL.
 
@@ -42,6 +47,7 @@ class SiteAPI(API):
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         return client.get_edit_node_www_url(node_name=node_name)
 
+    @handle_client_exceptions
     def get_site(self, site_id):
         """ Get description of a site.
 
@@ -50,6 +56,7 @@ class SiteAPI(API):
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         return client.get_site_from_id(site_id=site_id).json()
 
+    @handle_client_exceptions
     def get_compute(self, compute_id):
         """ Get description of a compute element.
 
@@ -58,6 +65,7 @@ class SiteAPI(API):
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         return client.get_compute(compute_id=compute_id).json()
 
+    @handle_client_exceptions
     def get_service(self, service_id):
         """ Get description of a service.
         
@@ -66,6 +74,7 @@ class SiteAPI(API):
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         return client.get_service(service_id=service_id).json()
 
+    @handle_client_exceptions
     def get_storage(self, storage_id):
         """ Get description of a storage resource.
 
@@ -74,6 +83,7 @@ class SiteAPI(API):
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         return client.get_storage(storage_id=storage_id).json()
 
+    @handle_client_exceptions
     def get_storage_area(self, storage_area_id):
         """ Get description of a storage area resource.
 
@@ -82,6 +92,7 @@ class SiteAPI(API):
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         return client.get_storage_area(storage_area_id=storage_area_id).json()
 
+    @handle_client_exceptions
     def list_compute(self, node_name=None, site_name=None):
         """ List compute elements across all sites. """
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
@@ -92,6 +103,7 @@ class SiteAPI(API):
             query_params["site_names"] = [site_name]
         return client.list_compute(**query_params).json()
 
+    @handle_client_exceptions
     def list_services(self, service_type=None, node_name=None, site_name=None, scope='all'):
         """ List services across all sites, with optional filters.
 
@@ -112,17 +124,20 @@ class SiteAPI(API):
 
         return client.list_services(**query_params).json()
 
+    @handle_client_exceptions
     def list_service_types(self):
         """ List supported service types. """
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         # services can be "core" or associated with a compute element, use combined result
         return client.list_service_types().json()
 
+    @handle_client_exceptions
     def list_sites(self):
         """ List sites. """
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
         return client.list_sites().json()
 
+    @handle_client_exceptions
     def list_storages(self, node_name=None, site_name=None):
         """ List storages across all sites. """
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
@@ -133,6 +148,7 @@ class SiteAPI(API):
             query_params["site_names"] = [site_name]
         return client.list_storages(**query_params).json()
 
+    @handle_client_exceptions
     def list_storage_areas(self, node_name=None, site_name=None):
         """ List storage areas across all sites. """
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
@@ -143,6 +159,7 @@ class SiteAPI(API):
             query_params["site_names"] = [site_name]
         return client.list_storage_areas(**query_params).json()
 
+    @handle_client_exceptions
     def list_storage_areas_topojson(self):
         """ List storage areas across all sites in topojson format. """
         client = self.session.client_factory.get_site_capabilities_client(is_authenticated=True)
